948e6f63fe806157be8cba3c2ad7f5404d233b17
LPedro20020 <pilopezvargas@knox.edu>
2024-05-06 02:43:32 -0500

    tailwind install, jerkyll update, home laidout etc

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_layouts/*.html',
+    './*.md',
+    './*.html',
+    './docs/*.html',
+    './js/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
