8997122e5d66fae27dea086b080ea0876c8f3863
brij-patel-knox <155894206+brij-patel-knox@users.noreply.github.com>
2024-05-26 16:43:52 -0500

    Revert "Update global styles, create header and footer, add icons"

--- js/calcSpan.js
+++
@@ -1,8 +0,0 @@
-// Calculate and set the height of the grid item spanning multiple rows
-const gridItems = document.querySelectorAll('.rowspan');
-gridItems.forEach(item => {
-    const rows = parseInt(item.dataset.rows);
-    const rowHeight = 148; // Height of each row
-    const rowGutter = 20;
-    item.style.height = `${rows * rowHeight + ((rows - 1) * rowGutter)}px`;
-});
