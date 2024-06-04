7f6ad11d1b801419d5b7392da95eb102958e0760
brij-patel-knox <bpatel@knox.edu>
2024-06-03 20:35:17 -0500

    shop header changes

---
+++ js/eyeFollow.js
@@ -0,0 +1,9 @@
+const cursors = document.querySelectorAll(".cursor");
+
+cursors.forEach((div, index) => {
+    window.addEventListener("mousemove", function (dets) {
+      div.style.left = `${dets.clientX}px`;
+      div.style.top = `${dets.clientY}px`;
+    });
+    div.style.transition = `${(index + 1) * 200}ms linear`; 
+  });
